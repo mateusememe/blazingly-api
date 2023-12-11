4e8f4cc3dbd49088614588d62e812bbec71caeae
leeozaka <leonardoanicetoneves@hotmail.com>
2023-12-11 15:41:58 -0300

    'stringying' html elements

--- index1.cpp
+++ index1.cpp
@@ -7,10 +7,12 @@ int main()
     //define your endpoint at the root directory
     CROW_ROUTE(app, "/")([]() {
         crow::mustache::set_base("html");
-        auto html = crow::mustache::load_text("index.html");
-        auto css = crow::mustache::load_text("css/style.css");
-        html += "<style>" + std::string(css) + "</style>";
-        return html;
+        auto head = crow::mustache::load_text("header.html");
+        auto page = crow::mustache::load_text("index.html");
+        auto css = crow::mustache::load_text("style.css");
+        //html += "<style>" + std::string(css) + "</style>";
+        //return page;
+        return head.append(css).append(page);
         });
 
 
